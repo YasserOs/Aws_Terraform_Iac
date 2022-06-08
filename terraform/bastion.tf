035031a4844832c6f6ec4d93369d3862c8bc4beb
YasserOsama <yasseros@yasser-ubu>
2022-06-08 22:15:50 +0200

    added ubuntu ami

--- terraform/bastion.tf
+++ terraform/bastion.tf
@@ -1,5 +1,21 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "bastion" {
-  ami           = var.ami # us-west-2
+  ami = data.aws_ami.ubuntu.id #ubuntu
   instance_type = "t2.micro"
   key_name = aws_key_pair.public_key.key_name
   provisioner "local-exec" {
